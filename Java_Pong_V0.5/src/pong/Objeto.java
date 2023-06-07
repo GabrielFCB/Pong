2f2cbd035dd51590f09c6ab05be4c56b9208e5ce
Gabriel <117751056+GabrielFCB@users.noreply.github.com>
2023-06-07 18:11:22 -0300

    Delete Java_Pong_V0.5 directory

--- Java_Pong_V0.5/src/pong/Objeto.java
+++
@@ -1,8 +0,0 @@
-package pong;
-
-public abstract class Objeto {
-    public int x;
-    public int y;
-    public int largura;
-    public int altura;
-}
--- Java_Pong_V0.5/src/pong/Renderizar.java
+++
@@ -1,8 +0,0 @@
-package pong;
-
-import java.awt.Graphics;
-
-public interface Renderizar {
-    public void desenhar(Graphics g);
-    public void atualizar();
-}
